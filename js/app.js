94ff0f306a3545e19aec33e2fd8dcf33f023c62b
Mikael Jergefelt <mikael.jergefelt@ki.se>
2018-05-28 20:12:36 +0200

    add more game logic

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 (function() {
   let level = 1,
-    lives = 3,
-    status = document.querySelector('[role="status"]');
+    lives = 3;
+
+  const status = document.querySelector('[role="status"]');
+  const main = document.querySelector('main');
+  const colorButtons = document.querySelectorAll('nav a');
 
   const displayLives = () => {
     return `<button>${lives} &#x2764;&#xFE0F;</button>`;
@@ -29,7 +32,7 @@
       setTimeout(() => {
         if (!document.querySelectorAll('.current.svg .color').length) {
           lives += 1;
-          status.innerHTML = '<p>Rätt!</p><button class="nextlevel">Nästa nivå &#x27A1;&#xFE0F;</button>';
+          status.innerHTML = '<p>Rätt! +1 &#x2764;&#xFE0F;</p><button class="nextlevel">Nästa nivå &#x27A1;&#xFE0F;</button>';
           status.className = 'correct next';
         } else {
           if (correct) {
@@ -50,7 +53,38 @@
     }
   };
 
-  let colorButtons = document.querySelectorAll('nav a');
+  const nextSvg = () => {
+    let randomChoice = Math.floor(Math.random() * svgs.length),
+      randomSvg = svgs[randomChoice];
+    if (randomSvg) {
+      let newSvg = document.createElement('div'),
+        currentSvg = document.querySelector('.svg.current');
+      newSvg.className = 'svg new';
+      newSvg.innerHTML = randomSvg;
+      main.appendChild(newSvg);
+      svgs.splice(randomChoice, 1);
+      if (currentSvg) {
+        currentSvg.classList.remove('current');
+        currentSvg.classList.add('done');
+        setTimeout(() => {
+          main.removeChild(currentSvg);
+        }, 600);
+      }
+      setTimeout(() => {
+        newSvg.classList.remove('new');
+        newSvg.classList.add('current');
+      }, 500);
+      Array.prototype.forEach.call(colorButtons, button => {
+        button.style.visibility = 'visible';
+      });
+      status.className = '';
+      setTimeout(() => {
+        status.className = 'status';
+        status.innerHTML = `<p>Nivå ${level}</p>${displayLives()}`;
+      }, 100);
+    }
+  };
+
   Array.prototype.forEach.call(colorButtons, button => {
     button.addEventListener('click', colorClick, false);
   });
@@ -59,7 +93,23 @@
     if (e.target.classList.contains('playagain')) {
       document.location.reload();
     }
+    if (e.target.classList.contains('nextlevel')) {
+      if (svgs && svgs.length) {
+        level += 1;
+        nextSvg();
+      } else {
+        status.innerHTML = '<p>Du klarade det!</p><button class="playagain">Spela igen &#x1F504;</button>';
+        status.className = 'correct gameover';
+      }
+    }
   });
   status.className = 'status';
   status.innerHTML = `<p>Nivå ${level}</p>${displayLives()}`;
+
+  if (svgs && svgs.length) {
+    nextSvg();
+  } else {
+    status.innerHTML = '<p>Något gick fel!</p><button class="playagain">Spela igen &#x1F504;</button>';
+    status.className = 'wrong gameover';
+  }
 })();
