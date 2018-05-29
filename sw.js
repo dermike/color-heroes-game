4fe33efd6f2d101de2066ccf6f23d086e46f8403
Mikael Jergefelt <mikael.jergefelt@ki.se>
2018-05-29 21:13:53 +0200

    add service worker

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    // after the service worker is installed,
+    // open a new cache
+    caches.open('my-pwa-cache').then(cache => {
+      // add all URLs of resources we want to cache
+      return cache.addAll([
+        './',
+        './index.html',
+        './css/style.css',
+        './js/svgs.min.js',
+        './js/app.min.js'
+      ]);
+    })
+  );
+});
