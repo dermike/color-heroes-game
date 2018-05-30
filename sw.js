1f59b7b0d2144232d466b79051c9e03cbe1079c7
Mikael Jergefelt <mikael.jergefelt@ki.se>
2018-05-30 10:33:09 +0200

    update service worker to cache and update

--- sw.js
+++ sw.js
@@ -1,16 +1,40 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    // after the service worker is installed,
-    // open a new cache
-    caches.open('my-pwa-cache').then(cache => {
-      // add all URLs of resources we want to cache
-      return cache.addAll([
-        './',
-        './index.html',
-        './css/style.css',
-        './js/svgs.min.js',
-        './js/app.min.js'
-      ]);
-    })
-  );
+let CACHE = 'cache-and-update';
+
+function precache() {
+  return caches.open(CACHE).then(function(cache) {
+    return cache.addAll([
+      './',
+      './index.html',
+      './css/style.css',
+      './js/svgs.min.js',
+      './js/app.min.js'
+    ]);
+  });
+}
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function(cache) {
+    return cache.match(request).then(function(matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  return caches.open(CACHE).then(function(cache) {
+    return fetch(request).then(function(response) {
+      return cache.put(request, response);
+    });
+  });
+}
+
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+  evt.waitUntil(precache());
+});
+
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
+  evt.waitUntil(update(evt.request));
 });
